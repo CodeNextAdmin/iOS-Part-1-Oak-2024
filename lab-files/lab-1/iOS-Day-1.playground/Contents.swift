2f3b5b0dfd4f9778301b33c03b7f246f3d42c673
Coach Olivia <olivia@csedge.org>
2024-08-28 15:36:59 -0700

    Restored so that all lab 1 challenges appear at once

--- lab-files/lab-1/iOS-Day-1.playground/Contents.swift
+++ lab-files/lab-1/iOS-Day-1.playground/Contents.swift
@@ -16,7 +16,7 @@ print("💡 Topic 1 💡\nCreating a String Variable & Changing Variables\nEdit 
 
 var name = "[EDIT ME]"
 
-print("> \(name) welcome to your first day coding in swift !\n")
+print("> \(name), welcome to your first day coding in swift !\n")
 
 
 print("\n🧠 Challenge 1: 🧠\nChange the existing variable (Age) to contain your age.\nEdit the code below this line 👇\n")
@@ -28,18 +28,17 @@ print("> I'm \(name) and I started learning Swift at \(age) years old.\n")
 
 print("-----------------------------------------------------------------------------------------\n")
 
-/*
+
 print("💡 Topic 2 💡\nCreating a Constant\nEdit the code below this line to contain Batman's name (Bruce Wayne) 👇\n")
 
 let batmansName = "[EDIT ME]"
 
 print("> \(batmansName) is the batman !\n")
-*/
 
 
 /*
 Changing a consant value will result in an error (ERROR)
-Uncomment (remove //) line 48 below to see the error.
+Uncomment (remove //) line 46 below to see the error.
  
 Cannot assign to value: 'batmansName' is a 'let' constant
 */
@@ -47,8 +46,8 @@ Cannot assign to value: 'batmansName' is a 'let' constant
 //batmansName = "Dick Grayson"
 
 
-// Uncomment below for challenge two:
-// print("\n🧠 Challenge 2: 🧠\nCreate a new constant called secondBatmanName, assign it \"Dick Grayson\", and print it.\nEdit the code below this line 👇\n")
+
+print("\n🧠 Challenge 2: 🧠\nCreate a new constant called secondBatmanName, assign it \"Dick Grayson\", and print it.\nEdit the code below this line 👇\n")
 
 /*
 👇 Your new code below 👇
@@ -58,18 +57,21 @@ Cannot assign to value: 'batmansName' is a 'let' constant
 
 print("-------------------------------------------------------------------------------------------")
 
-/*
+
 print("\n💡 Topic 3 💡 \nType Inference\nEdit the code below this line to put your favorite ice cream flavor 👇\n")
 
 var iceCreamFlavor = "[EDIT ME]"
 
 print("> \(iceCreamFlavor) is my favorite icecream flavor !\n\n")
 
+print("Check the type using type(of: )")
+print(type(of: iceCreamFlavor))
 
-print("🧠 Challenge 3: 🧠")
+
+print("\n\n🧠 Challenge 3: 🧠")
 
 print("Create a new set of variables for the iceCream's price (float or int), toppings (boolean), scoopCount (int)\nEdit the code below this line 👇\n")
-*/
+
 
 /*
 👇 Your new code below 👇
@@ -80,7 +82,7 @@ print("Create a new set of variables for the iceCream's price (float or int), to
 print("-------------------------------------------------------------------------------------------")
 
 
-/*
+
 print("\n💡 Topic 4 💡 \nType Annotation\nEdit the code below this line 👇\n")
 
 var phoneBrand: String = "[EDIT ME] Change this to your current phone brand e.g Google/Apple/Samsung"
@@ -97,14 +99,14 @@ print("\n🧠 Challenge 4A: 🧠 \nWhat will be the printed type output of the t
 var phoneBroken = false
 
 var phoneNumber = 123_555_6789
-*/
+
 
 /*
 👇 Your written answer below (use comments)👇
 */
 
 
-/*
+
 print("\n🧠 Challenge 4B: 🧠 \nType annotate the correct type for each variable/constant ?\nEdit the code below this line 👇\n")
 
 var idNumber = 717_11_1909
@@ -114,15 +116,15 @@ let carPrice = 55590.99
 let address = "345 Maiden Ln"
 
 var underWater =  true
-*/
+
 
 /*
 👇 Your new code below 👇
 */
 
 
-// Uncomment me first:
-//print("\n💡 Topic 4C 💡 \nType Safety\nUncomment the code to see type safety in action 👇\n")
+
+print("\n💡 Topic 4C 💡 \nType Safety\nUncomment the code to see type safety in action 👇\n")
 
 // var shoeSize: float = true
 
